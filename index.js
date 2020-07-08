f10c357a92be9e9865e21f3ba796e734ee3f0e30
Lupo Montero <lupomontero@gmail.com>
2020-07-08 11:50:41 -0500

    Agrega validatePhoneNumber

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ const getRandomCat = () => {
     }));
 };
 
-module.exports  = getRandomCat;
+const validatePhoneNumber = (input) => {
+    if (typeof input !== 'string') {
+        throw TypeError(`Esperaba string y recibí ${typeof input}`);
+    }
 
+    if (input.length !== 7) {
+        return false;
+    }
+    return true;
+};
 
-
-
-
-
-
-// const validatePhoneNumber = (input) => {
-//     if (typeof input !== 'string') {
-//         throw TypeError(`Esperaba string y recibí ${typeof input}`);
-//     }
-
-//     if (input.length !== 7) {
-//         return false;
-//     }
-//     return true;
-// };
-
-// module.exports = validatePhoneNumber;
+module.exports = {
+  getRandomCat,
+  validatePhoneNumber,
+};
