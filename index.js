7e81abd8b2735fda378ef79bf1d1bcf30febede9
Lupo Montero <lupomontero@gmail.com>
2020-07-08 11:59:57 -0500

    Descomenta validatePhoneNumber y otros...

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-const getRandomCat = () => {
-  return fetch('https://cataas.com/cat')
-    .then(resp => resp.blob())
-    .then(blob => ({
-      blob,
-    }));
+exports.getRandomCat = () => fetch('https://cataas.com/cat')
+  .then(resp => resp.blob())
+  .then(blob => ({
+    blob,
+  }));
+
+exports.validatePhoneNumber = (input) => {
+  if (typeof input !== 'string') {
+    throw TypeError(`Esperaba string y recibí ${typeof input}`);
+  }
+
+  if (input.length !== 7) {
+    return false;
+  }
+  return true;
 };
-
-module.exports  = getRandomCat;
-
-
-
-
-
-
-
-// const validatePhoneNumber = (input) => {
-//     if (typeof input !== 'string') {
-//         throw TypeError(`Esperaba string y recibí ${typeof input}`);
-//     }
-
-//     if (input.length !== 7) {
-//         return false;
-//     }
-//     return true;
-// };
-
-// module.exports = validatePhoneNumber;
